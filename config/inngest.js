535ebce0b8c8e7ca39ab6f2191b32b14606f6321
viyash-24 <viyashviyashan2417@gmail.com>
2025-11-22 16:44:47 +0530

    user sync creation function is created

--- config/inngest.js
+++ config/inngest.js
@@ -1,4 +1,27 @@
 import { Inngest } from "inngest";
+import User from "@/models/User"
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "E-Commerce-App-next" });
+
+//inngest Function to save user data  to a database
+export const syncUserCreation =inngest.createFunction(
+          {
+                    id:"sync-user-from-clerk"
+
+          },
+          {event:'clerk/user.created'},
+
+          async ({event}) =>{
+             const {id,first_name,last_name,email_addresses,image_url} =event.data
+             const userData = {
+                    _id:id,
+                    email:email_addresses[0].email_address,
+                    name:first_name+ '' +last_name,
+                    imageUrl:image_url
+             }
+             await connectDB()
+             await User.create(userData)
+
+          }
+)
