ec899a3dfb4626827eac9c0292ae4ace13c59988
viyash-24 <viyashviyashan2417@gmail.com>
2025-11-22 16:54:47 +0530

    user profile  update function is created

--- config/inngest.js
+++ config/inngest.js
@@ -25,3 +25,23 @@ export const syncUserCreation =inngest.createFunction(
 
           }
 )
+
+//inngest Function to update user data in database
+
+export const syncUserUpdation =inngest.createFunction(
+          {
+           id:'update-user-from-clerk'         
+          },
+          {event:'clerk/user.updated'},
+          async ({event}) =>{
+                    const {id,first_name,last_name,email_addresses,image_url} =event.data
+             const userData = {
+                    _id:id,
+                    email:email_addresses[0].email_address,
+                    name:first_name+ '' +last_name,
+                    imageUrl:image_url
+             }
+             await connectDB()
+             await User.findByIdAndUpdate(id,userData)
+          }
+)
