cd5958c38db661ec0687065a3d9c860a0de17ac6
viyash-24 <viyashviyashan2417@gmail.com>
2025-11-25 10:43:10 +0530

    seller product added function logic is implemented

--- app/api/product/add/route.js
+++ app/api/product/add/route.js
@@ -1,6 +1,7 @@
 import authSeller from "@/lib/authSeller";
 import { getAuth } from "@clerk/nextjs/server";
 import { v2 as cloudinary } from "cloudinary";
+import { resolve } from "styled-jsx/css";
 
 //configure cloudinary
 
@@ -19,6 +20,44 @@ export async function POST(request){
                               return NextResponse.json({success:false,message:'not authorized'})
                     }
                     const formData =await request.formData()
+
+                    const name=formData.get('name');
+                    const description=formData.get('description');
+                    const category=formData.get('category');
+                    const price=formData.get('price');
+                    const offerPrice=formData.get('offerPrice');
+
+                    const files=formData.getAll('images');
+
+                    if (!files || files.length === 0) {
+                              return NextResponse.json({success:false,message:'no files uploaded'})
+                    }
+
+                    const result =await Promise.all(
+                              files.map(async (file)=>{
+                                        const arrayBuffer =await file.arrayBuffer()
+                                        const buffer=Buffer.from(arrayBuffer)
+
+                                        return new Promise((resolve,reject)=>{
+                                                  const stream =cloudinary.uploader.upload_stream(
+                                                            {resource_type:'auto'},
+                                                            (error,result)=>{
+                                                                      if(error){
+                                                                                reject(error)
+                                                                      }else {
+                                                                                resolve(result)
+                                                                      }
+                                                            }
+                                                  )
+                                                  stream.end(buffer)
+                                        })
+
+                              })
+                    )
+
+             const image=result.map(result =>result.secure_url)
+
+
           } catch (error) {
                     
           }
