71f9f00027d0882e6974103228744e271a1dacae
viyash-24 <viyashviyashan2417@gmail.com>
2025-12-10 19:33:27 +0530

    seller authentication logic is corrected

--- app/api/product/seller-list/route.js
+++ app/api/product/seller-list/route.js
@@ -6,18 +6,17 @@ import { NextResponse } from "next/server"
 
 export async function GET(request){
           try {
-                  const {userId} =getAuth(request)
-                  const isSeller =  authSeller(userId)
+                  const { userId } = getAuth(request)
+                  const isSeller = await authSeller(userId)
 
                   if (!isSeller) {
-                    return NextResponse.json({success:false,message:'Not authorized'})
-                    
+                    return NextResponse.json({ success: false, message: 'Not authorized' }, { status: 401 })
                   }
                   await connectDB()
 
-                  const product =await Protect.find({})
-                  return NextResponse.json({success:true,products})
+                  const products = await Product.find({ userId })
+                  return NextResponse.json({ success: true, products }, { status: 200 })
           } catch (error) {
-                   return NextResponse.json({success:false,message:error.message}) 
+                   return NextResponse.json({ success: false, message: error.message }, { status: 500 }) 
           }
 }
