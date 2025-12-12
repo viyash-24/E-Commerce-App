912b09f0c95c7306e9f38f645de7147c3e5cb1e5
viyash-24 <viyashviyashan2417@gmail.com>
2025-12-12 21:24:05 +0530

    cart items update module created

---
+++ app/api/cart/update/route.js
@@ -0,0 +1,24 @@
+import connectDB from '@/config/db'
+import {getAuth } from '@clerk/nextjs/server'
+import User from '@/models/User'
+import { NextResponse } from 'next/server'
+
+
+
+export async function POST(request) {
+          try {
+                   const {userId} =getAuth(request)
+                   const {cartData} = await request.json()
+
+                   await connectDB()
+                   const user =await User.findById(userId)
+
+                   user.cartItems =cartData
+                  await user.save()
+
+                  return NextResponse.json({success:true});
+                    
+          } catch (error) {
+                return NextResponse.json({success:false,message:error.message})    
+          }
+}
