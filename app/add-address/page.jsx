25cc53c244d281958200c0b19a375d53a7f27759
viyash-24 <viyashviyashan2417@gmail.com>
2025-12-13 21:22:08 +0530

    toast package is imported

--- app/add-address/page.jsx
+++ app/add-address/page.jsx
@@ -5,6 +5,7 @@ import Footer from "@/components/Footer";
 import Image from "next/image";
 import { useState } from "react";
 import { useAppContext } from "@/context/AppContext";
+import toast from "react-hot-toast";
 
 const AddAddress = () => {
 
