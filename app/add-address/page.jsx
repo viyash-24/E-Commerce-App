a8c61de77d54ce71a5c79e92d9984d44217c3625
viyash-24 <viyashviyashan2417@gmail.com>
2025-12-13 21:36:48 +0530

    axios package is imported

--- app/add-address/page.jsx
+++ app/add-address/page.jsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { useState } from "react";
 import { useAppContext } from "@/context/AppContext";
 import toast from "react-hot-toast";
+import axios from "axios";
 
 const AddAddress = () => {
 
