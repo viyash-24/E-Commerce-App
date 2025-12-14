6d11cd5de7adf26ef4180e5e79db35011494a9d0
viyash-24 <viyashviyashan2417@gmail.com>
2025-12-14 12:07:08 +0530

    order placed function is created

---
+++ app/api/order/create/route.js
@@ -0,0 +1,50 @@
+import { inngest } from "@/config/inngest";
+import Product from "@/models/Protect";
+import User from "@/models/User";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+
+export async function POST(request) {
+          try {
+                 const {userId} =getAuth(request)  
+
+                 const {address,items}= await request.json()
+
+                 if (!address || items.length ===0) {
+                    return NextResponse.json({success:false,message:'Invalid data'})
+                 }
+
+                 // calculate amount using items
+                 const amount = await items.reduce(async(acc,item) =>{
+                    const product =await Product.findById(item.product)
+
+                    return acc + product.offerPrice * item.quantity
+                 },0)
+
+                 await inngest.send({
+                    name:'order/created',
+                    data:{
+                              userId,
+                              address,
+                              items,
+                              amount:amount +Math.floor(amount*0.02),
+                              date:Date.now()
+                    }
+
+                 })
+
+                 //clear user cart
+                 const user =await User.findById(userId)
+                 user.cartItems ={}
+                 await user.save()
+
+                 return NextResponse.json({success:true,message:'Order Placed'})
+
+
+
+          } catch (error) {
+                    return NextResponse.json({success:false,message:error.message})
+          }
+          
+}
