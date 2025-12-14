721ce4176fb720541bcc3f599090568aedba239d
viyash-24 <viyashviyashan2417@gmail.com>
2025-12-14 15:03:03 +0530

    rename product file imported

--- app/api/product/list/route.js
+++ app/api/product/list/route.js
@@ -1,5 +1,5 @@
 import connectDB from "@/config/db"
-import Product from "@/models/Protect"
+import Product from "@/models/Product"
 import { NextResponse } from "next/server"
 
 export async function GET(request){
