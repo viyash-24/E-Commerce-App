b399956e2960dc629f145c5b9a5d77b555cec2ee
viyash-24 <viyashviyashan2417@gmail.com>
2025-12-14 15:02:38 +0530

    await is added and product file imported

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,5 +1,5 @@
 import { inngest } from "@/config/inngest";
-import Product from "@/models/Protect";
+import Product from "@/models/Product";
 import User from "@/models/User";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
@@ -19,7 +19,7 @@ export async function POST(request) {
                  const amount = await items.reduce(async(acc,item) =>{
                     const product =await Product.findById(item.product)
 
-                    return acc + product.offerPrice * item.quantity
+                    return await acc + product.offerPrice * item.quantity
                  },0)
 
                  await inngest.send({
