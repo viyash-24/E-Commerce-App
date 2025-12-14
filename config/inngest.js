c29186898e54028206a82e70a4bb4748ea4aecc8
viyash-24 <viyashviyashan2417@gmail.com>
2025-12-14 12:23:11 +0530

    maxSize is reduce

--- config/inngest.js
+++ config/inngest.js
@@ -68,7 +68,7 @@ export const createUserOrder =inngest.createFunction(
        {
               id:'create-user-order',
               batchEvents:{
-                     maxSize:25,
+                     maxSize:5,
                      timeout:'5s'
               }
        },
