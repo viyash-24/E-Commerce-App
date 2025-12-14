981730d051a264e1852ebadc91baa656c86907a6
viyash-24 <viyashviyashan2417@gmail.com>
2025-12-14 15:01:53 +0530

    rename this file

--- models/Order.js
+++ models/Order.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
 const orderSchema= new mongoose.Schema({
-          userId:{type:String,required:true,ref:'user'},
-          iteams:[{
-                    product:{type:String,required:true,ref:'product'},
+          userId:{type:String,required:true, ref:'user'},
+          items:[{
+                    product:{type:String,required:true, ref:'product'},
                     quantity:{type:Number,required:true}
           }],
           amount:{type:Number,required:true},
           address:{type:String,required:true,ref:'address'},
-          status:{type:String,required:true,Default:'Order Placed'},
+          status:{type:String,required:true,default:'Order Placed'},
           date:{type:Number,required:true},
 })
 
