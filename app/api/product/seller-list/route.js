f0411ae91b7c4d7fdad8fbd544b8ae2e72fdd347
viyash-24 <viyashviyashan2417@gmail.com>
2025-12-14 15:03:13 +0530

    rename product file imported

--- app/api/product/seller-list/route.js
+++ app/api/product/seller-list/route.js
@@ -1,6 +1,6 @@
 import connectDB from "@/config/db"
 import authSeller from "@/lib/authSeller"
-import Product from "@/models/Protect"
+import Product from "@/models/Product"
 import { getAuth } from "@clerk/nextjs/server"
 import { NextResponse } from "next/server"
 
