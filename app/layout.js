f55489be9cad234f3e10b834f7c0229741f82ed5
viyash-24 <viyashviyashan2417@gmail.com>
2026-06-09 19:50:45 +0530

    updated title

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
 
 export const metadata = {
-  title: "QuickCart - GreatStack",
+  title: "QuickCart",
   description: "E-Commerce with Next.js ",
 };
 
