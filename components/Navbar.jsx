f35e50d7bf6f139364174d1eeb5c28f44c8f92a5
viyash-24 <viyashviyashan2417@gmail.com>
2026-06-09 19:49:36 +0530

    updated navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -24,12 +24,7 @@ const Navbar = () => {
         <Link href="/all-products" className="hover:text-gray-900 transition">
           Shop
         </Link>
-        <Link href="/" className="hover:text-gray-900 transition">
-          About Us
-        </Link>
-        <Link href="/" className="hover:text-gray-900 transition">
-          Contact
-        </Link>
+        
 
         {isSeller && (
           <button
