d3cedab9ac5794a89c9891a0594fadf47df165c6
viyash-24 <viyashviyashan2417@gmail.com>
2026-06-11 18:25:20 +0530

    route connection error fixed

--- app/api/cart/get/route.js
+++ app/api/cart/get/route.js
@@ -11,6 +11,9 @@ export async function GET(request) {
           await connectDB()
           const user=await User.findById(userId)
 
+          if (!user) {
+             return NextResponse.json({success:true ,cartItems:{}})
+          }
           const {cartItems} =user
 
           return NextResponse.json({success:true ,cartItems})
